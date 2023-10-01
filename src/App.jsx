35d8a2aa9d3a01f712097373bbc5f91d7b90cc5d
Antonio <abamorim36@gmail.com>
2023-10-01 16:21:50 -0300

    Created Component Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
 
 import './styles.css';
 
 export function App() {
   return (
     <div>
+    <Header />
+    
     <Post 
       author="Antonio Biasotti" 
       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aliquam impedit tenetur vero rerum, corrupti nihil repellendus sunt eos ex necessitatibus dolorum cupiditate rem totam laboriosam, hic nesciunt minus unde."
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export function Header() {
+     return (
+          <header className={styles.header}>
+               <strong>Social Feed</strong>
+          </header>
+     );
+}
