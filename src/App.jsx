eb4d89d36b0fa9868553fd502cddab09d864ca5a
Antonio <abamorim36@gmail.com>
2023-10-06 17:06:39 -0300

    Creating and Styling Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
     <Header />
     
-    <Post 
-      author="Antonio Biasotti" 
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aliquam impedit tenetur vero rerum, corrupti nihil repellendus sunt eos ex necessitatibus dolorum cupiditate rem totam laboriosam, hic nesciunt minus unde."
-    />
-    <Post
-      author="Gabriel Caruzo"
-      content="Um novo post muito legal"
-    />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+            <Post 
+              author="Antonio Biasotti" 
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aliquam impedit tenetur vero rerum, corrupti nihil repellendus sunt eos ex necessitatibus dolorum cupiditate rem totam laboriosam, hic nesciunt minus unde."
+            />
+            <Post
+              author="Gabriel Caruzo"
+              content="Um novo post muito legal"
+            />
+        </main>
+      </div>
     </div>
   )
 }
