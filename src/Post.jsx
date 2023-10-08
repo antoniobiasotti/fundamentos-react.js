2164c4354d9d11ee28a4334f0a62b046a476bce1
Antonio <abamorim36@gmail.com>
2023-10-08 20:45:58 -0300

    Creating Post Component

--- src/Post.jsx
+++
@@ -1,9 +0,0 @@
-export function Post(props) {
-     console.log(props);
-     return (
-     <div>
-          <strong>{props.author}</strong>
-          <p>{props.content}</p>
-     </div>
-     )
-}
---
+++ src/components/Post.jsx
@@ -0,0 +1,27 @@
+import styles from './Post.module.css';
+
+export function Post() {
+     return (
+          <article className={styles.post}>
+               <header>
+                    <div className={styles.author}>
+                         <img className={styles.avatar} src="https://github.com/antoniobiasotti.png"/>
+                         <div className={styles.authorInfo}>
+                              <strong>Antônio Biasotti</strong>
+                              <span>Web Developer</span>
+                         </div>
+                    </div>
+
+                    <time title="8 de Outubro às 20:37" dateTime="2023-08-10 20:36">Publicado há 1h</time>
+               </header>
+
+               <div className={styles.content}>
+                    <p>Fala galeraa 👋</p>
+                    <p>Acabei de subir mais um projeto pro meu portifólio!!</p>
+                    <p>Confere lá 🚀</p>
+                    <p>👉 <a href="#">antoniobiasotti.dev</a></p>
+                    <p><a href="">#novoprojeto #opentowork</a></p>
+               </div>               
+          </article>
+     )
+}
