f146338165a9bd917f82fbefbcf6c42727e30646
Antonio <abamorim36@gmail.com>
2023-10-11 19:53:19 -0300

    Created Component Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
           <article className={styles.post}>
                <header>
                     <div className={styles.author}>
-                         <img className={styles.avatar} src="https://github.com/antoniobiasotti.png"/>
+                         <Avatar src="https://github.com/antoniobiasotti.png"/>
                          <div className={styles.authorInfo}>
                               <strong>Antônio Biasotti</strong>
                               <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,7 +12,7 @@ export function Sidebar() {
                />
 
                <div className={styles.profile}>
-                    <img className={styles.avatar} src="https://github.com/antoniobiasotti.png" />
+                    <Avatar src="https://github.com/antoniobiasotti.png"/>
 
                     <strong>Antônio Biasotti</strong>
                     <span>Web Developer</span>
