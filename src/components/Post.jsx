f62805a8721bc77b3fc03b0a74768764f76ac20a
Antonio <abamorim36@gmail.com>
2023-10-16 07:05:28 -0300

    Adding key prop to Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,5 @@
 import { format, formatDistanceToNow } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR'
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
@@ -54,9 +54,9 @@ export function Post({ author, publishedAt, content }) {
                <div className={styles.content}>
                     {content.map(line => {
                          if (line.type === 'paragraph') {
-                              return <p>{line.content}</p>;
+                              return <p key={line.content}>{line.content}</p>;
                          } else if (line.type === 'link') {
-                              return <p><a href="">{line.content}</a></p>;
+                              return <p key={line.content}><a href="">{line.content}</a></p>;
                          }
                     })}
                </div>
@@ -78,8 +78,8 @@ export function Post({ author, publishedAt, content }) {
 
                <div className={styles.commentList}>
                     {comments.map(comment => {
-                         return <Comment content={comment}/>     
-                    })}
+                         return <Comment key={comment} content={comment}/>     
+                    })} 
                </div>
           </article>
      )
