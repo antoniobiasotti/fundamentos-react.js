4abb4953030425584b2818315a5afe8cad070870
Antonio <abamorim36@gmail.com>
2023-10-18 07:24:58 -0300

    Counting Likes Function

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,13 +1,18 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
 
-     function handleDeleteComment() {
-          console.log('deletar')
+     const [likeCount, setLikeCount] = useState(0);
 
+     function handleLikeComment() {
+          setLikeCount(likeCount + 1);
+     }
+
+     function handleDeleteComment() {
           onDeleteComment(content)
      }
 
@@ -32,9 +37,9 @@ export function Comment({ content, onDeleteComment }) {
                     </div>
 
                     <footer>
-                         <button>
+                         <button onClick={handleLikeComment}>
                               <ThumbsUp />
-                              Aplaudir <span>20</span>
+                              Aplaudir <span>{likeCount}</span>
                          </button>
                     </footer>
                </div>
